56615f710b71b6fb62453adae6e76ebbb69cd7da
PICKA6 <saravanaprakash2004@gmail.com>
2023-04-25 10:40:54 +0530

    Added Images

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -2,6 +2,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import categories from "../Persons.js";
+import { Box } from "@chakra-ui/react";
 const schema = z.object({
   date: z.string().min(3, { message: "Please Enter Atleast Three Characters" }),
   hours: z
@@ -26,59 +27,63 @@ const ExpenseForm = ({ onSubmit }: Props) => {
   } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
 
   return (
-    <form
-      onSubmit={handleSubmit((data) => {
-        onSubmit(data), reset();
-      })}
-    >
-      {" "}
-      <div className="mb-3">
-        <label htmlFor="person" className="form-label">
-          Persons
-        </label>
+    <Box ml="2vh" mt="2vh">
+      <form
+        onSubmit={handleSubmit((data) => {
+          onSubmit(data), reset();
+        })}
+      >
+        {" "}
         <div className="mb-3">
-          <select id="person" {...register("person")} className="form-select">
-            <option value=""></option>
-            {categories.map((person) => (
-              <option key={person} value={person}>
-                {person}
-              </option>
-            ))}
-          </select>
-        </div>
+          <label htmlFor="person" className="form-label">
+            Persons
+          </label>
+          <div className="mb-3">
+            <select id="person" {...register("person")} className="form-select">
+              <option value=""></option>
+              {categories.map((person) => (
+                <option key={person} value={person}>
+                  {person}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        {errors.person && (
-          <p className="text-danger">{errors.person.message}</p>
-        )}
-      </div>
-      <div className="mb-3">
-        <label htmlFor="date" className="form-label">
-          Date
-        </label>
-        <input
-          {...register("date")}
-          id="date"
-          type="text"
-          className="form-control"
-        />
-        {errors.date && <p className="text-danger">{errors.date.message}</p>}
-      </div>
-      <div className="mb-3">
-        <label htmlFor="hours" className="form-label">
-          Hours
-        </label>
-        <input
-          {...register("hours", { valueAsNumber: true })}
-          id="hours"
-          type="number"
-          className="form-control"
-        />
-        {errors.hours && <p className="text-danger">{errors.hours.message}</p>}
-      </div>
-      <button disabled={!isValid} type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+          {errors.person && (
+            <p className="text-danger">{errors.person.message}</p>
+          )}
+        </div>
+        <div className="mb-3">
+          <label htmlFor="date" className="form-label">
+            Date
+          </label>
+          <input
+            {...register("date")}
+            id="date"
+            type="text"
+            className="form-control"
+          />
+          {errors.date && <p className="text-danger">{errors.date.message}</p>}
+        </div>
+        <div className="mb-3">
+          <label htmlFor="hours" className="form-label">
+            Hours
+          </label>
+          <input
+            {...register("hours", { valueAsNumber: true })}
+            id="hours"
+            type="number"
+            className="form-control"
+          />
+          {errors.hours && (
+            <p className="text-danger">{errors.hours.message}</p>
+          )}
+        </div>
+        <button disabled={!isValid} type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </Box>
   );
 };
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,33 @@
-import { HStack, Text, Show, Button, Flex } from "@chakra-ui/react";
+import { HStack, Text, Show, Button, Flex, Icon } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import { useState } from "react";
+import { FcWorkflow } from "react-icons/fc";
+import React from "react";
 
 interface Props {
   children: string;
 }
 const NavBar = ({ children }: Props) => {
   const [value, setValue] = useState(" ");
-
+  const handleClick = () => {
+    window.location.reload();
+  };
   return (
     <Flex pos="sticky" justifyContent="space-between">
-      <Text fontFamily="sans-serif" padding="20px" marginY="10px">
-        {children}
-      </Text>
+      <HStack>
+        <Icon
+          boxSize="10vh"
+          padding="0.5"
+          mt="1em"
+          ml="3em"
+          onClick={handleClick}
+        >
+          <FcWorkflow />
+        </Icon>
+        <Text fontFamily="sans-serif" padding="20px" marginY="10px">
+          {children}
+        </Text>
+      </HStack>
       <HStack justifyContent="flex-end" padding="20px" gap="20px">
         <ColorModeSwitch />
       </HStack>
