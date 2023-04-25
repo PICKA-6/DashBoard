ebf457e053c7bc5edd019eeda70482b30059588d
PICKA6 <saravanaprakash2004@gmail.com>
2023-04-25 10:39:15 +0530

    Added Iamges Folder

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -5,7 +5,7 @@ const MyResponsivePie = () => (
   <ResponsivePie
     data={data}
     margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-    innerRadius={0.5}
+    innerRadius={0.4}
     padAngle={0.7}
     cornerRadius={3}
     activeOuterRadiusOffset={8}
